8580e3b9178b585f25aff185c4d1f8d5ebe2d031
gavvahar <nihargavva@gmail.com>
2023-04-16 01:22:33 -0400

    working on vscode

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
+
 class Hero(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
