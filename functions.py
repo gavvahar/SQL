200266df75a093d43af6fdb90cd93b373355f5f6
gavvahar <nihargavva@gmail.com>
2023-05-02 13:19:47 -0400

    Using Multiple Where()

--- functions.py
+++ functions.py
@@ -27,7 +27,7 @@ def create_heros():
 
 def select_heros():
     with Session(engine) as session:
-        statement = select(Hero).where(Hero.name != "Deadpond")
+        statement = select(Hero).where(Hero.age >= 35).where(Hero.age < 40)
         results = session.exec(statement)
         for hero in results:
             print(hero)
