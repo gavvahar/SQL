1081923a41e840f1cc5144e79c6de2aa28323e2a
gavvahar <nihargavva@gmail.com>
2023-05-02 13:21:58 -0400

    Alternative version of Multiple Expressions

--- functions.py
+++ functions.py
@@ -27,7 +27,7 @@ def create_heros():
 
 def select_heros():
     with Session(engine) as session:
-        statement = select(Hero).where(Hero.age >= 35).where(Hero.age < 40)
+        statement = select(Hero).where(Hero.age >= 35, Hero.age < 40)
         results = session.exec(statement)
         for hero in results:
             print(hero)
