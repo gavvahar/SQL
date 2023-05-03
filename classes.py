960b5ff57c6f18cbab82b22cfdc12b9db20264e0
gavvahar <nihargavva@gmail.com>
2023-05-03 14:53:15 -0400

    Connecting two Tables

--- classes.py
+++ classes.py
@@ -6,3 +6,10 @@ class Hero(SQLModel, table=True):
     name: str = Field(index=True)
     secret_name: str
     age: Optional[int] = Field(default=None, index=True)
+    team_id: Optional[int] = Field(default=None, foreign_key="team.id")
+
+
+class Team(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    headquarters: str
