79a369116317f518c3fb739df1c574e6c6fc9a0e
gavvahar <nihargavva@gmail.com>
2023-05-03 14:06:18 -0400

    dont need those lines

--- functions.py
+++ functions.py
@@ -25,20 +25,6 @@ def create_heroes():
         session.commit()
 
 
-# def update_heroes():
-#     with Session(engine) as session:
-#         statement = select(Hero).where(Hero.name == "Spider-Boy")
-#         results = session.exec(statement)
-#         hero = results.one()
-#         print("Hero:", hero)
-
-#         hero.age = 16
-#         session.add(hero)
-#         session.commit()
-#         session.refresh(hero)
-#         print("Updated hero:", hero)
-
-
 def update_heroes(name):
     with Session(engine) as session:
         statement = select(Hero).where(Hero.name == name)
